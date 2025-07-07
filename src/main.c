a355b42a021b0c9a64a01ad541a0ed845bd8d210
aoaochan <josejbeck99@yahoo.com>
2025-07-07 16:13:54 +0900

    Textures

--- src/main.c
+++ src/main.c
@@ -3,10 +3,13 @@
 #include <math.h>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image/stb_image.h>
 
 #include "shader.h"
 
 void framebufferSizeCallback(GLFWwindow *window, int width, int height);
+int loadTexture(unsigned int *texture, const char *path, int format);
 void processInput(GLFWwindow *window);
 
 const size_t WINDOW_WIDTH = 800, WINDOW_HEIGHT = 600;
@@ -35,26 +38,47 @@ int main(void) {
   shaderInit(&ourShader, "shaders/vertShader.glsl", "shaders/fragShader.glsl");
 
   float vertices[] = {
-     0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,
-     -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,
-      0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f
-  };
-  unsigned int VAO, VBO;
+    // positions                      // colors                        // texture coords
+     0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,           // top right
+     0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,     // bottom right
+    -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+    -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left 
+};
+unsigned int indices[] = {
+  0, 1, 3,
+  1, 2, 3
+};
+  unsigned int VAO, VBO, EBO;
   glGenVertexArrays(1, &VAO);
   glGenBuffers(1, &VBO);
+  glGenBuffers(1, &EBO);
 
   glBindVertexArray(VAO);
   
   glBindBuffer(GL_ARRAY_BUFFER, VBO);
   glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
   glEnableVertexAttribArray(0);
   
-  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+  glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
   glEnableVertexAttribArray(1);
 
+  glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+  glEnableVertexAttribArray(2);
+
+  unsigned int texture1, texture2;
+  stbi_set_flip_vertically_on_load(1);
+  if (loadTexture(&texture1, "textures/container.jpg", GL_RGB) != 0 || 
+  loadTexture(&texture2, "textures/awesomeface.png", GL_RGBA) != 0) return -1;
+
   shaderUse(&ourShader);
+
+  glUniform1i(glGetUniformLocation(ourShader.ID, "texture1"), 0);
+  shaderSetInteger(&ourShader, "texture2", 1);
 
   while (!glfwWindowShouldClose(window)) {
     processInput(window);
@@ -62,8 +86,14 @@ int main(void) {
     glClearColor(0.75f, 0.75f, 0.75f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glActiveTexture(GL_TEXTURE1);
+    glBindTexture(GL_TEXTURE_2D, texture2);
+
+    shaderUse(&ourShader);
     glBindVertexArray(VAO);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 
     glfwSwapBuffers(window);
     glfwPollEvents();
@@ -81,6 +111,30 @@ int main(void) {
 void framebufferSizeCallback(GLFWwindow *window, int width, int height) {
   (void)window;
   glViewport(0, 0, width, height);
+}
+int loadTexture(unsigned int *texture, const char *path, int format) {
+  glGenTextures(1, texture);
+  glBindTexture(GL_TEXTURE_2D, *texture);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  int width, height, nrChannels;
+  unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
+
+  if (!data) {
+    printf("Error: Failed to load texture: \"%s\"\n", path);
+    return 1;
+  }
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+  glGenerateMipmap(GL_TEXTURE_2D);
+
+  stbi_image_free(data);
+
+  return 0;
 }
 void processInput(GLFWwindow *window) {
   if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) glfwSetWindowShouldClose(window, true);
